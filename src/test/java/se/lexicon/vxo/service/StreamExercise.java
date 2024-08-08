1282be4dd161feb62c13ae25066bd03b4bb01932
AroniAri <meriton12345@gmail.com>
2024-08-08 11:10:18 +0200

    Exercise

--- src/test/java/se/lexicon/vxo/service/StreamExercise.java
+++ src/test/java/se/lexicon/vxo/service/StreamExercise.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.*;
 import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,7 +30,7 @@ public class StreamExercise {
     @Test
     public void task1(){
         List<Integer> integers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-        //TODO:Write code here
+        integers.stream().forEach( num -> System.out.println(num));
 
     }
 
@@ -38,9 +39,8 @@ public class StreamExercise {
      */
     @Test
     public void task2(){
-        long amount = 0;
+        long amount = people.stream().count();
 
-        //TODO:Write code here
 
         assertEquals(10000, amount);
     }
@@ -50,10 +50,11 @@ public class StreamExercise {
      */
     @Test
     public void task3(){
-        long amount = 0;
+        
         int expected = 90;
 
-        //TODO:Write code here
+        long amount = people.stream().filter(person ->"Andersson".equals(person.getLastName())).count();
+
 
         assertEquals(expected, amount);
     }
@@ -66,7 +67,10 @@ public class StreamExercise {
         int expectedSize = 4988;
         List<Person> females = null;
 
-        //TODO:Write code here
+
+        females= people.stream()
+                        .filter(person -> person.getGender() == Gender.FEMALE)
+                         .collect(Collectors.toList());
 
 
         assertNotNull(females);
@@ -81,7 +85,12 @@ public class StreamExercise {
         int expectedSize = 8882;
         Set<LocalDate> dates = null;
 
-        //TODO:Write code here
+        // create stream
+        // use map operation to map Person to BirthDate
+        // extract and make a TreSet as a result
+      dates =  people.stream()
+              .map( Person::getDateOfBirth)
+                        .collect(Collectors.toCollection(TreeSet::new));
 
         assertNotNull(dates);
         assertTrue(dates instanceof TreeSet);
